510b5c44776e4845a002d7c39f5113feb5ec98bd
ridhofp3 <ridhofp3@gmail.com>
2025-05-05 12:56:38 +0700

    Fix Model Kegiatan

--- app/Models/Kegiatan.php
+++ app/Models/Kegiatan.php
@@ -10,7 +10,15 @@ class Kegiatan extends Model
     use HasFactory;
 
     protected $table = 'kegiatan';
-    protected $fillable = ['nama_kegiatan', 'deskripsi', 'tanggal', 'waktu', 'lokasi', 'created_by'];
+    protected $fillable = [
+        'nama_kegiatan',
+        'gambar',
+        'deskripsi',
+        'tanggal',
+        'waktu',
+        'lokasi',
+        'created_by',
+    ];    
 
     // Relasi: Kegiatan dibuat oleh seorang User (Admin/Takmir)
     public function creator()
